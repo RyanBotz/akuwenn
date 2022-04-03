6350f57e447a01ee187c80e393b077ee3b0abb88
RyanBotz <98251598+RyanBotz@users.noreply.github.com>
2022-04-02 19:45:04 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body{
+            background-image: url('https://i.ibb.co/3Y05cHf/IMG-20220403-WA0123.jpg');
+            background-repeat: repeat-x;
+        }
+        h1{
+            color: white;
+        }
+    </style>
+    <title>Aku Wenn Group</title>
+</head>
+<body>
+    <center>
+        <div class="container mt-3">
+            <h1>Group JB Wenn</h1>
+        </div>
+        <div class="mt-3">
+            <a href="https://chat.whatsapp.com/Dj4fUd219AzJgDkEukx3AI" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 1</a>
+            <a href="https://chat.whatsapp.com/JhQMPUmPNiBEAOZI9D7cry" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 2</a>
+            <a href="https://chat.whatsapp.com/CaesumJT1nYEjCmZy6HJYh" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 3</a>
+            <a href="https://chat.whatsapp.com/HSmEiYbOeUH2VmBaRKOvLk" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 4</a>
+            <a href="https://chat.whatsapp.com/HkXPu6nPodEJo0JeNU0vaU" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 5</a>
+            <a href="https://chat.whatsapp.com/CzxfBUho0G76B0laiP0AeG" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 6</a>
+            <a href="https://chat.whatsapp.com/BCDNFJo4ldaCra71ppk9xB" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 7</a>
+            <a href="https://chat.whatsapp.com/KaG3ZrWBwij4Y46Xcb3oFR" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 8</a>
+            <a href="https://chat.whatsapp.com/LBO5MuwC0UHDiekRP0knMy" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 9</a>
+            <a href="https://chat.whatsapp.com/BEIUK2puUXSHFWuPjvuU3s" class="btn btn-success mt-3" style="width: 55%;">JB || Wenn 10</a>
+        </div>
+    </center>
+</body>
+</html>
