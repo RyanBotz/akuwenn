cd6bed33fd639eff955cd3f17c864689f059d4cd
RyanBotz <98251598+RyanBotz@users.noreply.github.com>
2022-04-02 19:49:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
         body{
-            background-image: url('https://i.ibb.co/3Y05cHf/IMG-20220403-WA0123.jpg');
+            background-image: url('https://i.ibb.co/ZTj0b5L/20220403-094222.png');
             background-repeat: repeat-x;
         }
         h1{
